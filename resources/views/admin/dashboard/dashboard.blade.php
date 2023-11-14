fa2ba08faf9939e9b12bcf7c9e812539637cdc13
stevenkw <steven.k21@mhs.istts.ac.id>
2023-11-14 10:22:50 +0700

    TEST BRANCH PUSH

--- resources/views/admin/dashboard/dashboard.blade.php
+++ resources/views/admin/dashboard/dashboard.blade.php
@@ -4,18 +4,19 @@
 @endsection
 
 @section('content')
+
 <body class="font-sans bg-gray-100">
 
-  <div class="flex">
-    @include('layout.admin-side')
-    <div class="flex-1">
-      
-      <div class="container mx-auto p-8">
-        <h1 class="text-4xl mb-4">Welcome, Admin</h1>
-      </div>
+    <div class="flex">
+        @include('layout.admin-side')
+        <div class="flex-1">
 
+            <div class="container mx-auto p-8">
+                <h1 class="text-4xl mb-4">Welcome, Admin</h1>
+            </div>
+
+        </div>
     </div>
-  </div>
 
 </body>
 @endsection
