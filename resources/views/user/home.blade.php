907df7ba7b34e5dad78305d929963c63ed04a3b2
Stevanus Hia <stevanushiaa@gmail.com>
2023-12-04 22:14:15 +0700

    indexig home 💀

--- resources/views/user/home.blade.php
+++ resources/views/user/home.blade.php
@@ -5,22 +5,24 @@
 
 @section('content')
 
-<div class="w-full bg-center bg-cover" style="background-image: url(https://images.pexels.com/photos/2707756/pexels-photo-2707756.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260);">
-<div class="flex items-center justify-center w-full h-full bg-gray-900 bg-opacity-50 py-48">
-  <div class="text-center">
-    <div class="container px-4 mx-auto">
-      <div class="max-w-4xl mx-auto text-center">
-        <span class="text-gray-200 font-semibold uppercase tracking-widest">New feature</span>
-        <h2 class="mt-8 mb-6 text-4xl lg:text-5xl font-bold text-gray-100">Lorem ipsum dolor sit amet consectetur
-          adipisicing elit.</h2>
-        <p class="max-w-3xl mx-auto mb-10 text-lg text-gray-300">
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laborum sit cum iure qui, nostrum at sapiente
-          ducimus.
-        </p>
-      </div>
+<div class="w-full bg-center bg-cover">
+    <div class="flex items-center justify-center w-full h-full bg-gray-900 bg-opacity-50 py-48">
+        <div class="text-center">
+            <div class="container px-4 mx-auto">
+                <div class="max-w-4xl mx-auto text-center">
+                    <span class="text-gray-200 font-semibold uppercase tracking-widest">NEW UPCOMING THEATRE</span>
+                    <h2 class="mt-8 mb-6 text-4xl lg:text-5xl font-bold text-gray-100">Lorem ipsum dolor sit amet
+                        consectetur
+                        adipisicing elit.</h2>
+                    <p class="max-w-3xl mx-auto mb-10 text-lg text-gray-300">
+                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laborum sit cum iure qui, nostrum at
+                        sapiente
+                        ducimus.
+                    </p>
+                </div>
+            </div>
+        </div>
     </div>
-  </div>
-</div>
 </div>
 
 <main class="container mx-auto mt-8">
@@ -29,12 +31,14 @@
 
     <div class="grid grid-cols-4 gap-2">
         @foreach ($plays as $play)
-          @if (isset($play['deleted_at']))
-              @continue
-          @endif
+        @if (isset($play['deleted_at']))
+        @continue
+        @endif
         <div>
             <a href="{{ route('toPlay', $play['id'] )}}">
-                <img src="{{ asset('storage/' . $play['poster']) }}" alt="{{ $play['title'] }} Poster" style="width: 200px" class="cursor-pointer image-modal-trigger" data-image-url="{{ asset('storage/' . $play['poster']) }}">
+                <img src="{{ asset('storage/' . $play['poster']) }}" alt="{{ $play['title'] }} Poster"
+                    style="width: 200px" class="cursor-pointer image-modal-trigger"
+                    data-image-url="{{ asset('storage/' . $play['poster']) }}">
             </a>
             {{$play['title']}}
         </div>
