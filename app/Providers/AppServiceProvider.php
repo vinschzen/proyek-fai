10187af6ec3ba8212ffd2a5e42fc8afd24c2c704
vin <vinshentwianata@gmail,com>
2023-12-09 00:34:28 +0700

    force https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,7 +23,7 @@ class AppServiceProvider extends ServiceProvider
         //
         Blade::directive('rupiah', function ( $expression ) { return "Rp. <?php echo number_format($expression,0,',','.'); ?>"; });
 
-        if (app()->environment('remote')) {
+        if (app()->environment('production')) {
             URL::forceScheme('https');
         }
     }
