3a98c7203ef88cecd8b416650e324a451a6619f5
Stevanus Hia <stevanushiaa@gmail.com>
2024-01-05 18:45:36 +0700

    dividing register page and login page

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -10,59 +10,11 @@
 
 <main class="container mx-auto mt-8">
     <div class="container mx-auto mt-8">
-        <div class="grid grid-cols-2 gap-2"> 
-            <div class="container">
-                <h2 class="text-2xl font-semibold mb-4">Register</h2>
-                <form class="max-w-md bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" method="POST"
-                    action="{{ route('register') }}">
-                    @csrf
-                    <div class="mb-4">
-                        <label class="block text-gray-700 text-sm font-bold mb-2" for="name">
-                            Name
-                        </label>
-                        <input
-                            class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                            id="name" type="text" placeholder="Your Name" name="username">
-                    </div>
-                    <div class="mb-4">
-                        <label class="block text-gray-700 text-sm font-bold mb-2" for="email">
-                            Email
-                        </label>
-                        <input
-                            class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                            id="email" type="email" placeholder="Email" name="email">
-                    </div>
-                    <div class="mb-6">
-                        <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
-                            Password
-                        </label>
-                        <input
-                            class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                            id="password" type="password" placeholder="******************" name="password">
-                    </div>
-                    <div class="mb-6">
-                        <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
-                            Confirm Password
-                        </label>
-                        <input
-                            class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                            id="password" type="password" placeholder="******************" name="password_confirmation">
-                    </div>
-                    <div class="flex items-center justify-between">
-                        <button
-                            class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-                            type="submit">
-                            Register
-                        </button>
-                    </div>
-                </form>
-
-            </div>
-            <div class="container">
-                <div class="container mx-auto mt-8">
-                    <h2 class="text-2xl font-semibold mb-4">Login</h2>
-                    <form class="max-w-md bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" method="POST"
-                        action="{{ route('login') }}">
+        <div class="flex items-center justify-center"> <!-- Centering the container -->
+        <div>
+            <h2 class="text-4xl font-semibold mb-4 text-center">Login</h2> <!-- Increased text size and centered -->
+                <div class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-14" >
+                    <form method="POST" action="{{ route('login') }}" style="width: 30vw">
                         @csrf
                         <div class="mb-4">
                             <label class="block text-gray-700 text-sm font-bold mb-2" for="email">
@@ -89,12 +41,9 @@
                         </div>
                     </form>
                 </div>
-
             </div>
-
         </div>
     </div>
-
 </main>
 
 @if (Session::has('msg'))
@@ -107,6 +56,6 @@
         confirmButtonText: 'OK'
     }).then((result) => {
     });  
-  </script>
+</script>
 @endif
 @endsection
