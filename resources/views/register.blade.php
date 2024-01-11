d69bf2100027263a28d3acc0a07f8e062d804324
Stevanus Hia <stevanushiaa@gmail.com>
2024-01-11 10:39:00 +0700

    bug register

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -54,7 +54,7 @@
                         <div class="relative mb-4" data-te-input-wrapper-init>
                         <input
                             type="text"
-                            name="username"
+                            name="name"
                             class="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
                             placeholder="Name" />
                         </div>
